b54f479702eef40c45f0ba86ac65f3f80efaf7bd
Justincee <calvertjustin1996@gmail.com>
2021-06-01 15:48:06 +0200

    updates

--- challenge.py
+++ challenge.py
@@ -1,16 +1,17 @@
+# importing the below modules so we can calculate the mean, standard deviation and Variance
 import numpy as np
 from numpy import *
+# Below is the range of numbers needed to do the calculations
 j = np.arange(20)
-
+# this is to print the array of numbers
 print("Original Array")
 print(j)
-
+# Below is the calculation and to print the result
 j_mean = np.mean(j)
 print("\nMean: ", j_mean)
-
+# Below is the calculation and to print the result
 j_std = np.std(j)
 print("\nStandard deviation: ", j_std)
-
+# Below is the calculation and to print the result
 j_var = np.var(j)
 print("\nVariance: ", j_var)
-
--- challenge2.py
+++ challenge2.py
@@ -1,13 +1,15 @@
+# importing the function below allows you to do the calculations you need to do
 import numpy as np
+# importing the below module is for the graph
 import matplotlib.pyplot as plt
-
+# below is the number and bins needed for the histogram
 nums = np.array([0.5, 0.7, 1.0, 1.2, 1.3, 2.1])
 bins = np.array([0, 1, 2, 3])
-
+# Below is to print the numbers, bins and results in the Terminal
 print("\nNumbers: ", nums)
 print("\nBins: ", bins)
 print("\nResult", np.histogram(nums, bins))
-
+# Below is the configuration for the histogram
 plt.hist(nums, bins=bins, color="yellow")
 plt.title("Challenge 2")
 plt.show()
